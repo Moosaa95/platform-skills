c62ddeb59a14882402bbf0c85e8fa3fb7442c7e4
moosa <moosaabdullahi45@gmail.com>
2025-02-27 17:11:11 +0100

    fix

--- backend/coronith/settings/base.py
+++ backend/coronith/settings/base.py
@@ -27,7 +27,7 @@ SECRET_KEY = getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 # DEBUG = getenv('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost', 'platform-skills.onrender.com').split(',')
+ALLOWED_HOSTS = getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
